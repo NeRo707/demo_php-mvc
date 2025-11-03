fcd2cd9839625181a9b26a965a295a4d8ee852a3
NeRo <99098290+NeRo707@users.noreply.github.com>
2025-11-03 20:58:47 +0400

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -17,6 +17,6 @@
         <a href="/api/tasks">API</a>
     </nav>
     <p>This is a basic MVC application with Composer autoloading.</p>
-    <p>Click on <strong>Tasks</strong> to see the task list!(t222222222t)</p>
+    <p>Click on <strong>Tasks</strong> to see the task list!(t131313131322t)</p>
 </body>
 </html>
